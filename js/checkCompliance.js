0fc95e7dba910ca3cce8d3e816d1a120a58e89db
Cooper Hanke <hanke31314@gmail.com>
2018-01-25 12:19:40 -0600

    Cleaned up the script

---
+++ js/checkCompliance.js
@@ -0,0 +1,29 @@
+function checkCompliance (username, password) {
+    console.log('Username: ' + username);
+    console.log('Password: ' + password);
+    var passwordLength, usernameLength, passwordCondition, noWhiteSpace;
+
+    if (password.length < 5){
+        console.log('Password is at least 5 characters');
+        passwordLength = true;
+    } else passwordLength = false;
+
+    if (username.length <= 20) {
+        console.log('username is less than or equal to 20 characters');
+        usernameLength = true;
+        } else usernameLength = false;
+
+    if (password.indexOf(username) < 0) {
+        passwordCondition = true;
+        } else passwordCondition = false;
+
+    if (
+        (username.trim() === username) && (password.trim() === password)
+        ) {
+            noWhiteSpace = true;
+            } else noWhiteSpace = false;
+
+    if (passwordLength && usernameLength && passwordCondition && noWhiteSpace) {
+        console.log('all conditions are now met');
+        } else console.log('one of the conditions is wrong, see above');
+}
