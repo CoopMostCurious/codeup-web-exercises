f3cbba7e52ac85ebb3da8d6abdcbf004ad4236ca
Cooper Hanke <hanke31314@gmail.com>
2018-01-29 14:39:02 -0600

    Added JavaScript related exercises

---
+++ js/external.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log('Hello from external JavaScript.');
+alert('Welcome to my Website!');
+var favoriteColor = prompt('What is your favorite color?');
+alert('Yes! ' + favoriteColor + ' is my favorite color as well... or is it?');
