db199de7d429890223f1b7ec5c671e3d730774e3
Cooper Hanke <hanke31314@gmail.com>
2018-01-30 16:03:24 -0600

    enhanced loops-bonus script

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,10 +4,15 @@ function skipNumber() {
     do {
         var numberToSkip = prompt('I can skip an odd number, 1 to 50 \(non-inclusive\). What number would you like to see me do that to?');
     } while ((numberToSkip % 2 === 0) || (typeof(parseInt(numberToSkip)) !== 'number'));
+    console.log('Number to skip: ' + numberToSkip);
     for (var i = 1; i < 50; i++) {
-        if ((i % 2 === 0) || (i === parseInt(numberToSkip))) {
+        if (i % 2 === 0) {
             continue;
-        } console.log('Here is a number: ' + i + '!');
+        } else if (i === parseInt(numberToSkip)) {
+            console.log('Oh snap! Skipping ' + numberToSkip + '!');
+            continue;
+        }
+        console.log('Here is a number: ' + i + '!');
     }
     alert('See? Looks for your number. It\'s not there. No worries, I\'ll wait...');
 }
