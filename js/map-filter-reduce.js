6693e4bbd604b3fa9f0b4d3b0aab3604ed47e49a
Cooper Hanke <hanke31314@gmail.com>
2018-02-20 23:59:41 -0600

    finished with map-filter-reduce exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,43 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+const triples =  users.filter(n => n.languages.length > 2);
+
+const emails = users.map(n => n.email);
+
+const usersAsObj = users.reduce((obj, item) => {
+        obj[item.id] = item;
+        return obj;
+    }, {});
+
+
